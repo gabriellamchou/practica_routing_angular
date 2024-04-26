1afc9508320afea08c946e3bd86e9443bfca7faf
Gabriel <gabriel.lamela.ch@gmail.com>
2024-04-26 13:12:40 +0200

    Acceder a ruta con un botón

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // Inyectamos Router
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  // con .navigate redirigimos al usuario
+  onLoadServers() {
+    // complex calculation
+    this.router.navigate(['/servers'])
   }
 
 }
